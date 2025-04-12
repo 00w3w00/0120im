cb21a018823b382cf04919435064ae59861b7796
최성현 <choisunghyun@choeseonghyeon-ui-Macmini.local>
2025-04-12 21:00:06 +0900

    chore: 브랜치 정리 및 라이브러리 정리

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<div className="">
-			<span className="text-5xl text-amber-800">Branch : WWW </span>
+			<span className="text-5xl text-blue-950">Branch : WWW </span>
 		</div>
 	)
 }
